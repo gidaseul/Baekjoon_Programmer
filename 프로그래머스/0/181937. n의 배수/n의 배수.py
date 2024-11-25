54ee4e38bd4a5beee478f80d37da8dabe8eea55b
gidaseul <61573303+gidaseul@users.noreply.github.com>
2024-11-26 01:17:00 +0900

    [level 0] Title: n의 배수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181937. n의 배수/n의 배수.py
+++ 프로그래머스/0/181937. n의 배수/n의 배수.py
@@ -1,5 +1,4 @@
 def solution(num, n):
-    answer = 0
-    if (num % n) == 0:
-        answer =1
-    return answer
+  
+# num%n은 int 값이지만, 이를 not() 함수 안에 넣으면 int가 bool로 해석되어서 num%n이 0이면 False로, 0이 아니면 True로 해석되면서 1을 반환한다.
+    return int(not(num % n))
