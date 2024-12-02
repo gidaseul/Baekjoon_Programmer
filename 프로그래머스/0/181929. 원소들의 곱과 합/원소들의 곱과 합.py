200bcabb9aa8c31dabaedc6ea70a7cd4471ad479
gidaseul <61573303+gidaseul@users.noreply.github.com>
2024-12-03 03:15:24 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    a = 1
+    b = 0
+    for i in range(len(num_list)):
+        a *= num_list[i]
+        b += num_list[i]
+    if a < (b**2):
+        return 1 
+    else:
+        return 0
