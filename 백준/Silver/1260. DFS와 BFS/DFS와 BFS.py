85aafeebcc16db55bf9e6dc8f26a5242165e3f15
gidaseul <61573303+gidaseul@users.noreply.github.com>
2025-03-21 20:58:26 +0900

    [Silver II] Title: DFS와 BFS, Time: 120 ms, Memory: 39308 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,39 @@
+import sys
+def dfs(idx):
+    global visited
+    visited[idx] = True
+    print(idx,end=' ')
+    for next in range(1, N+1):
+        if not visited[next] and graph[idx][next]:
+            dfs(next)
+
+def bfs():
+    global q, visited
+    while q:
+        cur = q.pop(0)
+        print(cur, end=' ')
+        for next in range(1,N+1):
+            if not visited[next] and graph[cur][next]:
+                visited[next] = True
+                q.append(next)
+        
+
+N,M,V = map(int,sys.stdin.readline().split())
+
+graph = [[False] * (N+1) for _ in range(N+1)]
+visited = [False] * (N+1)
+
+# 그래프 업데이트
+for _ in range(M):
+    a,b = map(int,sys.stdin.readline().split())
+    graph[a][b] = True
+    graph[b][a] = True
+    
+ #DFS
+dfs(V)
+print() # 줄바꿈
+
+visited = [False] * (N+1)
+q = [V]
+visited[V] = True
+bfs()
