a03e2db8c99a3593904c31ea15deef33e7cceb97
gidaseul <61573303+gidaseul@users.noreply.github.com>
2025-04-03 14:46:00 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 684 ms, Memory: 120716 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -5,9 +5,14 @@ N = sys.stdin.readline()
 n = list(map(int, sys.stdin.readline().split()))
 
 M = sys.stdin.readline()
-m = list(map(int,sys.stdin.readline().split()))
+m = list(map(int, sys.stdin.readline().split()))
+
 counter_n = Counter(n)
+
+result = []  # 결과를 저장할 리스트
 
 for i in m:
     frequency = counter_n[i]
-    print(frequency)
+    result.append(frequency)  # 빈도수를 리스트에 추가
+
+print(*result)  # 리스트의 요소들을 공백으로 구분하여 출력
