be19f9e59a4a92b2b9465804018a6f09fd51f06b
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-01-28 22:10:57 +0900

    Title: 달에서 무게 구하기, Time: 97ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260128/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        float weight = 13f;
+        float gravityRatio = 0.165f;
+
+        System.out.printf("%.0f * %.6f = %.6f", weight,gravityRatio,(weight*gravityRatio));
+
+    }
+}
