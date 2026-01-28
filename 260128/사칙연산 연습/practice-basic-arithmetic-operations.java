312a2dce46dcc2e2f8c85cf8d65c754b00d29f21
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-01-28 21:26:30 +0900

    Title: 사칙연산 연습, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260128/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5;
+        int b = 30;
+        int c = a+b;
+        int quotient = c/8;
+        int remainder = c%8;
+
+        System.out.println(quotient);
+        System.out.println(remainder);
+
+    }
+}
